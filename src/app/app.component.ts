f93832bd2e5040274621aca75160848f698859ec
Chris Wainner <chris.jwainner@gmail.com>
2018-04-20 19:30:13 -0700

    Update current-forecast link to hide/show component on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SkyCast Weather';
+
+  toggleForecast(){
+    console.log("Click!");
+    $('#currentForecast').toggle();
+  }
 }
