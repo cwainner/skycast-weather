5c659b4f25c12cc79cc9187ef66380dd4f26b6ff
Chris Wainner <chris.jwainner@gmail.com>
2018-04-20 18:57:57 -0700

    Add current-forecast component, add grid for sidebar and sub-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CurrentForecastComponent } from './current-forecast/current-forecast.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentForecastComponent
   ],
   imports: [
     BrowserModule
